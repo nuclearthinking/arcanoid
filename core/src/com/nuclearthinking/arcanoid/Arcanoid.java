0e9ab62087fda8037f4544e4435d40b1826585a7
Vladislav Radchenko <onifent@gmail.com>
2016-03-13 23:08:56 +0300

    Старт проекта

---
+++ core/src/com/nuclearthinking/arcanoid/Arcanoid.java
@@ -0,0 +1,30 @@
+package com.nuclearthinking.arcanoid;
+
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Arcanoid extends Game {
+
+    SpriteBatch batch;
+    BitmapFont font;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        this.setScreen(new GameScreen(this));
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+    }
+}
---
+++ core/src/com/nuclearthinking/arcanoid/GameScreen.java
@@ -0,0 +1,52 @@
+package com.nuclearthinking.arcanoid;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Date: 12.03.2016
+ * Time: 18:21
+ *
+ * @author Vladislav Radchenko (onfient@gmail.com)
+ */
+public class GameScreen implements Screen {
+    final Arcanoid mainGame;
+
+    public GameScreen(final Arcanoid mainGame){
+        this.mainGame = mainGame;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
