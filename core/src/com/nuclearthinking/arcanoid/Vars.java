091d03b3e7756d7fb248f88e48faa1c1c35cb011
Vladislav Radchenko <onifent@gmail.com>
2016-03-16 00:08:03 +0300

    добавил арканоид на экран, реализовал перемещение арканоида

--- core/src/com/nuclearthinking/arcanoid/Vars.java
+++ core/src/com/nuclearthinking/arcanoid/Vars.java
@@ -6,10 +6,12 @@ package com.nuclearthinking.arcanoid;
  *
  * @author Vladislav Radchenko (onfient@gmail.com)
  */
-public class Variables {
+public class Vars {
 
     public static final String TITLE = "Arcanoid";
     public static final int WIDTH = 800;
     public static final int HEIGHT = 460;
     public static final int TOPMENU_HEIGHT = 34;
+    public static final int ARCANOID_WIDTH = 120;
+    public static final int ARCANOID_HEIGHT = 21;
 }
